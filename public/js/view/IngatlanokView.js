4c805c087b09896ba86b1bdf37e28674e3e61de1
Blaj3n <benceulrich@gmail.com>
2023-04-27 12:40:28 +0200

    javítani kell

--- public/js/view/IngatlanokView.js
+++ public/js/view/IngatlanokView.js
@@ -1,13 +1,13 @@
 class IngatlanokView {
     #elem;
     constructor(elem, szuloelem) {
-        console.log("IngatlanokView Hali!");
+        console.log("IngatlanokView Hali!", elem);
         this.#elem = elem;
         szuloelem.append(`
         <div class="ingatlan">
             <h2> ID: ${elem.id}</h2>
-            <h2> Kategória: ${elem.kategoria}</h2>
-            <h2> Leírás: ${elem.leiras}</h2>
+            <h2> Kategória: ${elem.Kategória}</h2>
+            <h2> Leírás: ${elem.Leírás}</h2>
             <h2> Hírdetés dátuma: ${elem.hirdetesDatuma}</h2>
             <h2> Tehermentes: ${elem.tehermentes}</h2>
             <h2>Fénykép: </h2><img src="${elem.fenykep}">
