82f4154725fe974c4c7abcf4b95baadc610e90a3
Blaj3n <benceulrich@gmail.com>
2023-04-27 10:53:37 +0200

    kész Controllerek

--- app/Http/Controllers/IngatlanokController.php
+++ app/Http/Controllers/IngatlanokController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ingatlanok;
 use Illuminate\Http\Request;
 
 class IngatlanokController extends Controller
 {
-    //
+    public function index()
+    {
+        $ingatlanoks = Ingatlanok::all();
+        return $ingatlanoks;
+    }
+    public function show($id)
+    {
+        $ingatlanoks = Ingatlanok::find($id);
+        return $ingatlanoks;
+    }
+    public function destory($id){
+        Ingatlanok::find($id)->delete();
+    }
+    public function store(Request $request){
+        $Ingatlanok = new Ingatlanok();
+        $Ingatlanok->id = $request->id;
+        $Ingatlanok->kategoria = $request->kategoria;
+        $Ingatlanok->leiras = $request->leiras;
+        $Ingatlanok->hirdetesDatuma = $request->hirdetesDatuma;
+        $Ingatlanok->tehermentes = $request->tehermentes;
+        $Ingatlanok->ar = $request->ar;
+        $Ingatlanok->kepUrl = $request->kepUrl;
+        $Ingatlanok->save();
+    }
+    public function update(Request $request, $id){
+        $Ingatlanok = Ingatlanok::find($id);
+        $Ingatlanok->id = $request->id;
+        $Ingatlanok->kategoria = $request->kategoria;
+        $Ingatlanok->leiras = $request->leiras;
+        $Ingatlanok->hirdetesDatuma = $request->hirdetesDatuma;
+        $Ingatlanok->tehermentes = $request->tehermentes;
+        $Ingatlanok->ar = $request->ar;
+        $Ingatlanok->kepUrl = $request->kepUrl;
+        $Ingatlanok->save();
+    }
 }
--- app/Http/Controllers/KategoriakController.php
+++ app/Http/Controllers/KategoriakController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kategoriak;
 use Illuminate\Http\Request;
 
 class KategoriakController extends Controller
 {
-    //
+    public function index()
+    {
+        $kategoriaks = Kategoriak::all();
+        return $kategoriaks;
+    }
+    public function show($id)
+    {
+        $kategoriaks = Kategoriak::find($id);
+        return $kategoriaks;
+    }
+    public function destroy($id)
+    {
+        Kategoriak::find($id)->delete();
+    }
+    public function store(Request $request)
+    {
+        $Kategoriak = new Kategoriak();
+        $Kategoriak->id = $request->id;
+        $Kategoriak->nev = $request->nev;
+    }
+    public function update(Request $request, $id)
+    {
+        $Kategoriak = Kategoriak::find($id);
+        $Kategoriak->id = $request->id;
+        $Kategoriak->nev = $request->nev;
+    }
 }
